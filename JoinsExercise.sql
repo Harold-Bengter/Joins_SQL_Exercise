ee5d21d1c61ee8b73fc8023d0661fdc3f90368ce
Harold Bengter <Haroldbe55@yahoo.com>
2022-08-01 10:38:12 -0400

    Made ccr changes

--- JoinsExercise.sql
+++ JoinsExercise.sql
@@ -1,34 +1,33 @@
 /* joins: select all the computers from the products table:
 using the products table and the categories table, return the product name and the category name */
  SELECT products.Name, categories.Name
-FROM products
-INNER JOIN categories
-ON products.CategoryID = categories.CategoryID; 
+FROM products p
+INNER JOIN categories c
+ON p.CategoryID = c.CategoryID; 
 
 /* joins: find all product names, product prices, and products ratings that have a rating of 5 */
- SELECT products.Name, products.Price, reviews.Rating
-FROM products
-LEFT JOIN reviews
-ON products.ProductID = reviews.ProductID
-WHERE reviews.Rating = 5; 
+ SELECT p.Name, p.Price, r.Rating
+FROM products p
+LEFT JOIN reviews r
+ON p.ProductID = r.ProductID
+WHERE r.Rating = 5; 
 
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
-SELECT employees.FirstName, employees.MiddleInitial, employees.LastName, SUM(sales.Quantity) AS "Total Sales"
-FROM employees
-LEFT JOIN sales ON employees.EmployeeID = sales.EmployeeID
-GROUP BY employees.FirstName,  employees.MiddleInitial, employees.LastName
-ORDER BY SUM(sales.Quantity) DESC; 
+SELECT e.FirstName, e.MiddleInitial, e.LastName, SUM(s.Quantity) AS "Total Sales"
+FROM employees e
+LEFT JOIN sales s ON e.EmployeeID = s.EmployeeID
+GROUP BY e.FirstName,  e.MiddleInitial, e.LastName
+ORDER BY SUM(s.Quantity) DESC; 
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
 SELECT
-	departments.Name,
-    categories.Name
-FROM
-	departments
-JOIN categories
-	ON departments.DepartmentID = categories.DepartmentID
+departments.Name,
+categories.Name
+FROM departments d
+JOIN categories c
+ON d.DepartmentID = c.DepartmentID
 HAVING categories.Name = "Games"
-	OR categories.Name = "Appliances";
+OR categories.Name = "Appliances";
     
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
@@ -52,7 +51,7 @@ This query should return:
 -  the name of each product
 -  and how many of that product they sold */
 SELECT employees.EmployeeID, employees.FirstName, employees.LastName, products.Name AS "Product Name", SUM(sales.Quantity) AS "Quantity Sold"
-FROM products
-INNER JOIN sales ON products.ProductID = sales.ProductID
-INNER JOIN employees ON employees.EmployeeID = sales.EmployeeID
-GROUP BY employees.EmployeeID, employees.FirstName, employees.LastName, products.Name;
+FROM products p
+INNER JOIN sales s ON p.ProductID = s.ProductID
+INNER JOIN employees e ON e.EmployeeID = s.EmployeeID
+GROUP BY e.EmployeeID, e.FirstName, e.LastName, p.Name;
